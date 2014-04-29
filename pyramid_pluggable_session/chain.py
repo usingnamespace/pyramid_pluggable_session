4907ec8c36b2fbef8f5de36191ead8110408dc4c
Bert JW Regeer <bertjw@regeer.org>
2014-04-29 00:29:42 -0600

    Allow plugs to be chained

---
+++ pyramid_pluggable_session/chain.py
@@ -0,0 +1,56 @@
+import logging
+log = logging.getLogger(__name__)
+
+from pyramid.settings import aslist
+
+from zope.interface import implementer
+
+from .interfaces import IPlugSession
+
+@implementer(IPlugSession)
+class _ChainSessionPlug(object):
+    """ Chain based session
+
+    This allows you to chain various session plugs, for example local memory,
+    memcache, database and then file system. This way we can try fastests to
+    slowest in order.
+    """
+    def __init__(self, plugs):
+        self.plugs = plugs
+
+    def loads(self, session, request):
+        for plug in self.plugs:
+            sdata = plug.loads(session, request)
+
+            if sdata:
+                return sdata
+
+
+    def dumps(self, session, request, sess_data):
+        for plug in self.plugs:
+            plug.dumps(session, request, sess_data)
+
+
+required_settings = [
+        'pluggable_session.chain.plugs',
+        ]
+
+def ChainSessionPlug(config):
+    for _require in required_settings:
+        if _require not in config.registry.settings:
+            raise RuntimeError(_require + ' needs to be set.')
+
+    plugs = aslist(config.registry.settings['pluggable_session.chain.plugs'], flatten=False)
+
+    dotted_plugs = []
+    for plug in plugs:
+        plug = config.maybe_dotted(plug)
+        dotted_plugs.append(plug(config))
+
+
+    return _ChainSessionPlug(dotted_plugs)
+
+def includeme(config):
+    config.registry.registerUtility(ChainSessionPlug(config), IPlugSession)
+
+
