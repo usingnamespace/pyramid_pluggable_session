57f0147b716a03cff33dce6c6c7f5eff549e44f6
Bert JW Regeer <bertjw@regeer.org>
2014-04-29 00:50:18 -0600

    Prepare for 0.0.0a1

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ testing_extras = tests_require + [
     ]
 
 setup(name='pyramid_pluggable_session',
-      version='0.0.0dev',
+      version='0.0.0a1',
       description='A pluggable session implementation for Pyramid',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
